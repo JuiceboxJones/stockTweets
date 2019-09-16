298aab730a7464150a129c94c595756e7d8cb52d
Juicebox Jones <jderhalli@gmail.com>
2019-09-15 19:06:58 -0700

    Updates

---
+++ precache-manifest.c87d230d4097607c1e0150fc56fffc81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "310503100e2347d1b50b1274848360b6",
+    "url": "/stockTweets/index.html"
+  },
+  {
+    "revision": "c025727a1d9beb518cfb",
+    "url": "/stockTweets/static/css/main.7c56e5ae.chunk.css"
+  },
+  {
+    "revision": "1def623ec82af8011dba",
+    "url": "/stockTweets/static/js/2.35d135a3.chunk.js"
+  },
+  {
+    "revision": "c025727a1d9beb518cfb",
+    "url": "/stockTweets/static/js/main.7dfa347f.chunk.js"
+  },
+  {
+    "revision": "dc3a4977c1193bc2fbbf",
+    "url": "/stockTweets/static/js/runtime~main.e5a7a724.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stockTweets/static/media/logo.5d5d9eef.svg"
+  }
+]);
